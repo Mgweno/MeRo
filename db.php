0ab971b1859c16024c1e853aea171a1605902bcf
Mgweno <mgweno.ramadhan@yahoo.com>
2026-07-28 19:42:09 +0300

    Change database connection to Aiven MySQL
    Updated database connection settings to use Aiven MySQL with SSL options.

--- db.php
+++ db.php
@@ -1,12 +1,25 @@
 <?php
-$host = '127.0.0.1';
-$dbname = 'retail_db';
-$username = 'root';
-$password = '';
+$host = 'mysql-29497d54-mcnoopar-ee3c.e.aivencloud.com';
+$port = '28668';
+$dbname = 'defaultdb';
+$username = 'avnadmin';
+$password = 'AVNS_aVZpPralnEh3REL5ZP';
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $dsn = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4";
+    
+    // Aiven requires SSL connection options
+    $options = [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::MYSQL_ATTR_SSL_CA => __DIR__ . '/ca.pem', // Optional if you download the CA certificate, but usually works with options below
+    ];
+
+    // Alternatively, if you just want to connect without managing the certificate file locally right away:
+    $pdo = new PDO($dsn, $username, $password, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8mb4"
+    ]);
+
 } catch (PDOException $e) {
     die("Database connection failed: " . $e->getMessage());
 }
